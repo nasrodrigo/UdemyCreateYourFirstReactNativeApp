109418af85c216b68780b93bf9ee3e90d7748dd1
Rodrigo Monteiro <rmonteiro@obsglobal.com>
2021-09-08 11:32:43 -0500

    Added tab navigation

---
+++ src/screens/Contacts/ContactNew.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const ContactNew = () => {
+  return (
+    <View>
+      <Text>"New Contact"</Text>
+    </View>
+  );
+};
+
+export default ContactNew;
